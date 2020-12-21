e63daad15d7036add51da1382418e1f395ace899
Shota Tamura <tamura@yuuniworks.com>
2020-12-21 18:04:03 +0900

    seo

---
+++ src/features/foundation/DefaultSeo.tsx
@@ -0,0 +1,8 @@
+import { NextSeo } from 'next-seo';
+import React from 'react';
+
+export const DefaultSeo: React.FC = () => {
+  return (
+    <NextSeo title="Nanoid-Gen" description="Simple online nanoid generator" />
+  );
+};
