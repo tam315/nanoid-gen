d306d667d417a444be2c9397030cf107da8ad3be
Shota Tamura <tamura@yuuniworks.com>
2026-08-07 13:07:25 +0900

    Prettierの適用

--- src/features/foundation/DefaultSeo.tsx
+++ src/features/foundation/DefaultSeo.tsx
@@ -1,9 +1,11 @@
-import Head from "next/head";
+import Head from 'next/head';
 import React from 'react';
 
 export const DefaultSeo: React.FC = () => {
-  return <Head>
-    <title>Nano ID Generator</title>
-    <meta name="description" content="Generate multiple Nano IDs online" />
-  </Head>;
+  return (
+    <Head>
+      <title>Nano ID Generator</title>
+      <meta name="description" content="Generate multiple Nano IDs online" />
+    </Head>
+  );
 };
